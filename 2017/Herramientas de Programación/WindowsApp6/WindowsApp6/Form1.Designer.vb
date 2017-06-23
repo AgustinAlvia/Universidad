c6f57cb9d0a7aa1a20e91d68061d0232c8824ffb
Agustín Alvia <AgustinAlvia@users.noreply.github.com>
2017-06-23 09:07:36 -0500

    fisrt deployment

---
+++ 2017/Herramientas de Programación/WindowsApp6/WindowsApp6/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbxNumeros = New System.Windows.Forms.ListBox()
+        Me.btnEjecutar = New System.Windows.Forms.Button()
+        Me.lblMayor = New System.Windows.Forms.Label()
+        Me.lblMenor = New System.Windows.Forms.Label()
+        Me.lblMedio = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lbxNumeros
+        '
+        Me.lbxNumeros.FormattingEnabled = True
+        Me.lbxNumeros.Location = New System.Drawing.Point(74, 71)
+        Me.lbxNumeros.Name = "lbxNumeros"
+        Me.lbxNumeros.Size = New System.Drawing.Size(120, 95)
+        Me.lbxNumeros.TabIndex = 0
+        '
+        'btnEjecutar
+        '
+        Me.btnEjecutar.Location = New System.Drawing.Point(12, 12)
+        Me.btnEjecutar.Name = "btnEjecutar"
+        Me.btnEjecutar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEjecutar.TabIndex = 1
+        Me.btnEjecutar.Text = "Ejecutar"
+        Me.btnEjecutar.UseVisualStyleBackColor = True
+        '
+        'lblMayor
+        '
+        Me.lblMayor.AutoSize = True
+        Me.lblMayor.Location = New System.Drawing.Point(12, 181)
+        Me.lblMayor.Name = "lblMayor"
+        Me.lblMayor.Size = New System.Drawing.Size(39, 13)
+        Me.lblMayor.TabIndex = 2
+        Me.lblMayor.Text = "Label1"
+        '
+        'lblMenor
+        '
+        Me.lblMenor.AutoSize = True
+        Me.lblMenor.Location = New System.Drawing.Point(71, 181)
+        Me.lblMenor.Name = "lblMenor"
+        Me.lblMenor.Size = New System.Drawing.Size(39, 13)
+        Me.lblMenor.TabIndex = 3
+        Me.lblMenor.Text = "Label2"
+        '
+        'lblMedio
+        '
+        Me.lblMedio.AutoSize = True
+        Me.lblMedio.Location = New System.Drawing.Point(128, 181)
+        Me.lblMedio.Name = "lblMedio"
+        Me.lblMedio.Size = New System.Drawing.Size(39, 13)
+        Me.lblMedio.TabIndex = 4
+        Me.lblMedio.Text = "Label3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.lblMedio)
+        Me.Controls.Add(Me.lblMenor)
+        Me.Controls.Add(Me.lblMayor)
+        Me.Controls.Add(Me.btnEjecutar)
+        Me.Controls.Add(Me.lbxNumeros)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbxNumeros As ListBox
+    Friend WithEvents btnEjecutar As Button
+    Friend WithEvents lblMayor As Label
+    Friend WithEvents lblMenor As Label
+    Friend WithEvents lblMedio As Label
+End Class
---
+++ 2017/Herramientas de Programación/WindowsApp6/WindowsApp6/Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class Form1
+    Dim numeros As New List(Of Integer)
+
+    Sub actualizar()
+        Dim mayor As Integer
+        Dim menor As Integer
+        Dim medio As Double
+
+        numeros.Sort()
+
+        mayor = numeros.Max()
+        menor = numeros.Min()
+
+        medio = numeros.Count / 2
+        'MsgBox(Math.Floor(medio))
+
+        lblMayor.Text = "Mayor: " & mayor
+        lblMenor.Text = "Menor: " & menor
+
+        If (numeros.Count Mod 2 <> 0 And numeros.Count > 1) Then
+
+            lblMedio.Text = "Medio: " & numeros.Item(Math.Floor(medio))
+
+
+            'lblMedio.Text = "Medio : " & lbxNumeros.Items.Item(Math.Floor(medio))
+        Else
+            lblMedio.Text = "No tiene medio:"
+
+        End If
+
+
+    End Sub
+
+    Private Sub btnEjecutar_Click(sender As Object, e As EventArgs) Handles btnEjecutar.Click
+        Dim numero As Integer
+        numero = 1
+
+        While (numero <> 0)
+            numero = InputBox("Ingrese un número: ")
+            If (numero < 0) Then
+                MsgBox("Ingreso un número negativo: ")
+            End If
+
+            If (numero <> 0) Then
+                numeros.Add(numero)
+
+                actualizar()
+
+                lbxNumeros.DataSource = {}
+                lbxNumeros.DataSource = numeros
+
+            End If
+        End While
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
